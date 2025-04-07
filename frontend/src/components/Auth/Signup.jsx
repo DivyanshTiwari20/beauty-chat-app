042c3266d30fa45b821937f3a49a697556da943a
divyansh20 <tmadhuvijay1982@gmail.com>
2025-04-07 23:20:32 +0530

    Full stack working app | Needed better frontend | V1.1

--- frontend/src/components/Auth/Signup.jsx
+++ frontend/src/components/Auth/Signup.jsx
@@ -12,7 +12,7 @@ export default function Signup() {
   const handleSignup = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:5000/api/auth/signup', {
+      await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/signup`, {
         username,
         email,
         password,
