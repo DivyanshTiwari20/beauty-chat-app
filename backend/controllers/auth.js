1961f7510068df4240003892afe6726de5d1fd35
divyansh20 <tmadhuvijay1982@gmail.com>
2025-04-13 09:16:47 +0530

    updated the time limit for jwt token -v1.4

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -72,7 +72,7 @@ exports.login = async (req, res) => {
     const token = jwt.sign(
       { id: user._id }, 
       process.env.JWT_SECRET,
-      { expiresIn: '1h' }
+      { expiresIn: '15d' }
     );
 
     res.status(200).json({ 
